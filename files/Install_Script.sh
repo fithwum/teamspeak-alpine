e11cefc0aab8725dec11957e38f91872507f6bfb
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-02 15:45:26 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -16,6 +16,7 @@ if [ -e "${CHANGELOG}" ]
 			sleep 1
 			echo "Clearing old teamspeak files and preserving settings/logs/userfiles."
 			echo "(this will take some time if you have uploaded many/large files)"
+			shopt -s extglob
 			rm -frv !(files|logs|*.ini|*.sh) /ts3server/
 #			cp -R /ts3server/files/. /ts3temp/serverfiles/files/
 #			cp -R /ts3server/logs/. /ts3temp/serverfiles/logs/
@@ -25,6 +26,7 @@ if [ -e "${CHANGELOG}" ]
 #			cp -R /ts3temp/serverfiles/. /ts3server/
 #			rm -fr /ts3temp/serverfiles/*
 			sleep 1
+			shopt -u extglob
 			wget --no-cache https://files.teamspeak-services.com/releases/server/${TS_VERSION}/teamspeak3-server_linux_alpine-${TS_VERSION}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 			sleep 1
 			tar -xf /ts3temp/ts3server_${TS_VERSION}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
