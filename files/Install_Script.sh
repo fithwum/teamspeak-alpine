7652a26b30a568636adce2d1ab1ede2737cb3edb
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-01 21:53:38 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -5,11 +5,13 @@
 # Teamspeak server version check.
 TS_VERSION="3.5.1"
 CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION}
-CHANGELOG_OLD=find ../ts3server -type f -iname "CHANGELOG_[0-9].[0-9].[0-9]"
+CHANGELOG_OLD="../ts3server/CHANGELOG_[0-9].[0-9].[0-9]"
 CHANGELOG_NEW=CHANGELOG_${TS_VERSION}
 # VERSION_CHECK=
+
 chown 99:100 -R /ts3server
 chmod 777 -R /ts3server
+
 # Main Install (alpine).
 if [ "${CHANGELOG_OLD}" = CHANGELOG_"${TS_VERSION}" ]
 	then
