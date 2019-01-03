be804d7e1aed1e433e313d51cff876f9d2ff2740
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-03 11:44:47 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -2,20 +2,19 @@
 # Copyright (c) 2018 fithwum
 # All rights reserved
 
+# Variables.
 echo "Checking server version from teamspeak."
 wget --no-cache https://www.teamspeak.com/versions/server.json -O /ts3temp/server.json
 TS_VERSION_CHECK=$(cat /ts3temp/server.json | grep version | head -1 | awk -F: '{print $4}' | sed 's/[",]//g' | sed "s/checksum//g")
-echo "Latest server version from teamspeak:$TS_VERSION_CHECK"
-
-# Variables.
-TS_VERSION="3.5.1"
-CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION_CHECK}
+TS_VERSION=${TS_VERSION_CHECK}
+CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION}
+echo "Latest server version from teamspeak:$TS_VERSION"
 
 # Main install (alpine).
 # Check for files in /ts3server and download/create if needed.
 if [ -e "${CHANGELOG}" ]
 	then
-		echo "INFO ! ts3server is ${TS_VERSION_CHECK} ... checking that ini/sh files exist before running current docker."
+		echo "INFO ! ts3server is ${TS_VERSION} ... checking that ini/sh files exist before running current docker."
 		rm -frv /ts3temp/server.json
 	else
 		echo "WARNING ! ts3server is out of date ... will download new copy from teamspeak."
@@ -30,12 +29,12 @@ if [ -e "${CHANGELOG}" ]
 			cp -R /ts3temp/serverfiles/. /ts3server/
 			rm -fr /ts3temp/serverfiles/*
 			sleep 1
-			wget --no-cache https://files.teamspeak-services.com/releases/server/${TS_VERSION_CHECK}/teamspeak3-server_linux_alpine-${TS_VERSION_CHECK}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION_CHECK}.tar.bz2
+			wget --no-cache https://files.teamspeak-services.com/releases/server/${TS_VERSION}/teamspeak3-server_linux_alpine-${TS_VERSION}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 			sleep 1
-			tar -xf /ts3temp/ts3server_${TS_VERSION_CHECK}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
+			tar -xf /ts3temp/ts3server_${TS_VERSION}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
 			sleep 1
 			rm -fr /ts3temp/serverfiles/ts3server_startscript.sh
-			rm -frv /ts3temp/ts3server_${TS_VERSION_CHECK}.tar.bz2
+			rm -frv /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 			cp -uR /ts3temp/serverfiles/. /ts3server/
 			sleep 1
 			mv /ts3server/redist/libmariadb.so.2 /ts3server/libmariadb.so.2
